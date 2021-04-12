3f853289b222ac32e27059486e471d9360bdc7da
mnabeelkhan1985 <mnabeelkhan1985@gmail.com>
2021-04-13 02:44:23 +0500

    Javascript assignment No.1

---
+++ app.js
@@ -0,0 +1,16 @@
+alert("Hello, Mr Bean \n How are you?");
+
+
+var message ="Hello World";
+alert(message);
+
+
+var age=35;
+alert(age);
+
+
+var birthyear=1985;
+document.writeln(birthyear);
+
+var book="A smarter way to learn Javascript";
+document.writeln(book);
